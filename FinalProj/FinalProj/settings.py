bdf6c7f0f221400d13511ef3ccf86b27c3bff6f3
Gregory Maltezos <G.Maltezos@acg.edu>
2023-12-06 19:55:04 +0200

    attempt2 of making django admin work deployed

--- FinalProj/FinalProj/settings.py
+++ FinalProj/FinalProj/settings.py
@@ -127,7 +127,8 @@ LOGOUT_REDIRECT_URL = '/'
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILE_DIRS = [os.path.join(BASE_DIR, "static"),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
